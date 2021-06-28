84f2eb19359e74a60b405edbeb0a71872d2cb318
Anton Shestakov <prradd@gmail.com>
2021-06-29 00:06:01 +0300

    more unit tests added

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,21 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {TestBed, inject} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
-import { WeatherService } from './weather.service';
+import {WeatherService} from './weather.service';
+import {IWeather} from '../types';
 
 describe('WeatherService', async () => {
-  let service: WeatherService;
+  let weatherService: WeatherService;
+  let httpTestingController: HttpTestingController;
+  let weather: IWeather;
+  let baseUrl = "https://api.openweathermap.org/data/2.5/weather?appid=1890fcd579ac51bb12f42af3ceed6428&units=metric&id=123.";
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      providers: [ WeatherService ],
-      imports: [ HttpClientTestingModule ],
+      providers: [WeatherService],
+      imports: [HttpClientTestingModule],
     });
-    service = TestBed.inject(WeatherService);
+
+    weather = {
+      weather: [{
+        id: 123,
+        main: "test",
+        description: "test description",
+        icon: "01n"
+      }],
+      main: {
+        temp: 35.38,
+        feels_like: 35.38,
+        pressure: 1006,
+        humidity: 78
+      },
+      name: "test name",
+    };
+    httpTestingController = TestBed.get(HttpTestingController);
+    weatherService = TestBed.inject(WeatherService);
 
   });
+
+  beforeEach(inject(
+    [WeatherService],
+    (service: WeatherService) => {
+      weatherService = service;
+    }
+  ));
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(weatherService).toBeTruthy();
   });
+
+  it("should return data", () => {
+    let result = {};
+    weatherService.fetchWeather(123).subscribe(t => {
+      result = t;
+    });
+    const req = httpTestingController.expectOne({
+      method: "GET",
+      // url: baseUrl
+    });
+
+    console.log(req.request.url);
+
+    req.flush(weather);
+
+    expect(result).toEqual(weather);
+  });
+
 });
+
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,19 +5,16 @@ import {IWeather} from "../types";
 
 const appId = '1890fcd579ac51bb12f42af3ceed6428';
 const units = 'metric';
-const apiUrl= `https://api.openweathermap.org/data/2.5/weather?appid=${appId}&units=${units}&`;
+export const apiUrl= `https://api.openweathermap.org/data/2.5/weather?appid=${appId}&units=${units}&`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  private apiUrl =
-    'https://api.openweathermap.org/data/2.5/weather?appid=1890fcd579ac51bb12f42af3ceed6428&units=metric&';
-
   constructor(private http: HttpClient) { }
 
   fetchWeather(cityId: number): Observable<IWeather> {
-    return this.http.get<IWeather>(`${this.apiUrl}id=${cityId}`)
+    return this.http.get<IWeather>(`${apiUrl}id=${cityId}`)
   }
 }
